e5afc5d2c55149753dbdcbc0a508c5840562bd3c
Ese Udom <udomese@yahoo.com>
2023-07-08 13:52:03 +0100

    Revert "Initial commit containing mostly UI, and Room, Hilt-DI, ZXing implementations. There is also an APK file in app/debug for testing on devices for those who can't test from an IDE."
    This reverts commit 68e58b4c2cbfa23af2ce7a0b3088a04cccd2fd50.

--- app/src/main/java/org/scidsg/hushline/android/model/Message.kt
+++
@@ -1,9 +0,0 @@
-package org.scidsg.hushline.android.model
-
-import android.os.Parcelable
-import kotlinx.parcelize.Parcelize
-
-@Parcelize
-data class Message(val timestamp: String,
-                   val message: String,
-                   val read: Boolean) : Parcelable
