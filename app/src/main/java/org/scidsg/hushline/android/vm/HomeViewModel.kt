4f98caa62b5e602e979a46d3ae00a9977826fbb1
Ese Udom <udomese@yahoo.com>
2023-07-31 14:06:49 +0100

    Modify home model view

--- app/src/main/java/org/scidsg/hushline/android/vm/HomeViewModel.kt
+++ app/src/main/java/org/scidsg/hushline/android/vm/HomeViewModel.kt
@@ -5,8 +5,10 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import org.briarproject.android.dontkillmelib.DozeUtils
+import org.scidsg.hushline.android.UIState
 import org.scidsg.hushline.android.database.SettingsEntity
 import org.scidsg.hushline.android.repo.SettingsRepository
 import javax.inject.Inject
@@ -22,10 +24,20 @@ class HomeViewModel @Inject constructor(
     private val _reuseAddressStatusLiveData = repository.reuseStatus
     val reuseAddressStatusLiveData: LiveData<SettingsEntity> = _reuseAddressStatusLiveData
 
+    private val _runState = repository.runState
+    val runState: StateFlow<UIState> = _runState
+
+    val needsDozeWhitelisting get() = DozeUtils.needsDozeWhitelisting(applicationContext)
+
+    //todo Call this inside repository instead
     fun setRunningStatus(value: Boolean) {
         viewModelScope.launch {
             repository.setRunningStatus(value)
         }
+    }
+
+    fun runHushLine() = viewModelScope.launch {
+        repository.startHushLineRun()
     }
 
     fun setReuseStatus(value: Boolean) {
