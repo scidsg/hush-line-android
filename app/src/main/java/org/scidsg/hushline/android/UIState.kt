fbc5739586df670d79e30df297017f1f5db3e756
Ese Udom <udomese@yahoo.com>
2023-07-31 14:08:46 +0100

    Add new state class for UI

--- app/src/main/java/org/scidsg/hushline/android/UIState.kt
+++ app/src/main/java/org/scidsg/hushline/android/UIState.kt
@@ -1,4 +1,26 @@
 package org.scidsg.hushline.android
 
-class UIState {
+sealed class UIState {
+
+    data class Starting(
+        private val torPercent: Int
+    ) : UIState() {
+        init {
+            require(torPercent in 0..100)
+        }
+    }
+
+    data class Started(
+        val onionAddress: String
+    ) : UIState()
+
+    data class Stopping(
+        val onionAddress: String
+    ) : UIState()
+
+    object Stopped : UIState()
+
+    data class Error(
+        val torFailedToConnect: Boolean = false,
+    ) : UIState()
 }
